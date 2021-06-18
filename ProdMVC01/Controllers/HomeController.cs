282d495060871fc081b0f4800e4d9da4dd2e22df
LucasBara-dising <lucas.barauna098@gmail.com>
2021-06-18 14:49:09 -0300

    Add project files.

---
+++ ProdMVC01/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProdMVC01.Models;
+
+namespace ProdMVC01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var prod = new Produto();
+            return View(prod);
+        }
+        [HttpPost]
+
+        public ActionResult Index(Produto prod)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", prod);
+            }
+
+            return View(prod);
+        }
+
+        public ActionResult Resultado(Produto prod)
+        {
+            return View(prod);
+        }
+    }
+}
---
+++ ProdMVC01/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace ProdMVC01.Models
+{
+    public class Produto
+    {
+        [Display(Name ="Codigo")]
+        [Required(ErrorMessage ="O Código é obrigatorio")]
+        public int CodigoProd { get; set; }
+
+        [Display(Name = "Nome do Produto")]
+        [Required(ErrorMessage = "O nome do produto é obrigatorio")]
+        [StringLength(80, ErrorMessage ="O Nome esta muito longo")]
+        public string NomeProd { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "A descrição do produto é obrigatoria")]
+        [StringLength(150, ErrorMessage = "A descrição do produto esta muito longa")]
+        public string DescProd { get; set; }
+
+        [Display(Name = "Peso(g)")]
+        [Required(ErrorMessage = "O peso é obrigatorio")]
+        public int PesoProd { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "A categoria do produto é obrigatorio")]
+        [StringLength(80, ErrorMessage = "A descrição do produto esta muito longa")]
+        public string CategoriaProd { get; set; }
+
+        [Required(ErrorMessage = "O valor é obrigatorio")]
+        [Display(Name = "Valor")]
+        //[DisplayFormat(DataFormatString = "{0:N}", ApplyFormatInEditMode = true)]
+        public decimal ValorUni { get; set; }
+    }
+}
